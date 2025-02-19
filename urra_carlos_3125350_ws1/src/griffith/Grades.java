c43981bb85b96def12b9e7382a59dd69b694d9b5
Carlos-0620 <carlosdavidu8@gmail.com>
2025-02-19 16:49:38 +0000

    The method gradesMax(int[] grades) works perfectly fine. I will continue to test the following method

--- urra_carlos_3125350_ws1/src/griffith/Grades.java
+++ urra_carlos_3125350_ws1/src/griffith/Grades.java
@@ -3,7 +3,14 @@ package griffith;
 public class Grades {
 
 	static int gradesMax(int[] grades) {
-		return 0;
+		if (grades.length == 0) throw new IllegalArgumentException("Grade array cannot be empty.");
+        int max = grades[0];
+        for (int grade : grades) {
+            if (grade > max) {
+                max = grade;
+            }
+        }
+        return max;
 		
 	}
 	
