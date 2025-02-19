dede2947a915d3946c9ce59ebe2a135d91e4a0bb
Carlos-0620 <carlosdavidu8@gmail.com>
2025-02-19 17:04:33 +0000

    I implemented the method GradesTotal(int[] grades)and the unit test works succesfully. I will implement the following method now

--- urra_carlos_3125350_ws1/src/griffith/Grades.java
+++ urra_carlos_3125350_ws1/src/griffith/Grades.java
@@ -14,18 +14,23 @@ public class Grades {
 		
 	}
 	
-	 static int gradesTotal (int[] grades) {
-		return 0;
+	static int gradesTotal (int[] grades) {
+		if (grades.length == 0) throw new IllegalArgumentException("Grade array cannot be empty.");
+        int total = 0;
+        for (int grade : grades) {
+            total += grade;
+        }
+        return total;
 		 
-	 }
+	}
 	 
-	 static double gradesAverage(int[] grades) {
+	static double gradesAverage(int[] grades) {
 		return 0.0;
 		 
-	 }
+	}
 	 
-	 static int countFails(int[] grades, int minGrade) {
+	static int countFails(int[] grades, int minGrade) {
 		return 0;
 		 
-	 }
+	}
 }
