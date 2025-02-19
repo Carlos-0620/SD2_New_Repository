0b93cac545118cf61238a512889e927ce0ce93c3
Carlos-0620 <carlosdavidu8@gmail.com>
2025-02-19 15:31:15 +0000

    I Created a JUnit test case called GradesTest.java and a java file called Grades.java

---
+++ urra_carlos_3125350_ws1/src/griffith/Grades.java
@@ -0,0 +1,24 @@
+package griffith;
+
+public class Grades {
+
+	int gradesMax(int[] grades) {
+		return 0;
+		
+	}
+	
+	 static int gradesTotal (int[] grades) {
+		return 0;
+		 
+	 }
+	 
+	 static double gradesAverage(int[] grades) {
+		return 0;
+		 
+	 }
+	 
+	 static int countFails(int[] grades, int minGrade) {
+		return minGrade;
+		 
+	 }
+}
